dd72b8c807da0e9eab209fe6482381a389cff7b1
Gleb Karpushkin <rugleb@gmail.com>
2018-10-20 15:09:47 +0300

    moved application to another file

---
+++ cad/application.py
@@ -0,0 +1,60 @@
+from PyQt5.QtWidgets import QMainWindow, QAction, QDesktopWidget, QMessageBox
+
+
+class Application(QMainWindow):
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+        self._setMenuBar()
+        self._setStatusBar()
+        self._setGeometry()
+
+        self.setWindowTitle('Workspace')
+        self.show()
+
+    def _setMenuBar(self):
+        menu = self.menuBar()
+        file = menu.addMenu('File')
+        file.addAction(self._exitAction())
+        file.addAction(self._openAction())
+        file.addAction(self._saveAction())
+
+    def _exitAction(self):
+        action = QAction('Exit', self)
+        action.setShortcut('Ctrl+Q')
+        action.setStatusTip('Exit application')
+        action.triggered.connect(self.close)
+        return action
+
+    def _saveAction(self):
+        action = QAction('Save As', self)
+        action.setShortcut('Ctrl+S')
+        action.setStatusTip('Saving')
+        return action
+
+    def _openAction(self):
+        action = QAction('Open', self)
+        action.setShortcut('Ctrl+O')
+        action.setStatusTip('Open file')
+        return action
+
+    def _setStatusBar(self):
+        self.statusBar().showMessage('Ready')
+
+    def _setGeometry(self):
+        desktop = QDesktopWidget()
+        self.setGeometry(desktop.availableGeometry())
+
+    def closeEvent(self, event):
+        title = 'Close application'
+        question = 'Are you sure you want to quit?'
+
+        default = QMessageBox.No
+        buttons = QMessageBox.No | QMessageBox.Yes
+
+        answer = QMessageBox.question(self, title, question, buttons, default)
+        if answer == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
