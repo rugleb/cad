c4fdfda001b5fb6c62197188edb2de10b7a1fbcf
Gleb Karpushkin <rugleb@gmail.com>
2018-10-24 01:04:04 +0300

    created basic constraints

---
+++ cad/2d/constraints.py
@@ -0,0 +1,26 @@
+class Constraint:
+    pass
+
+
+class Coincident(Constraint):
+    pass
+
+
+class Parallel(Constraint):
+    pass
+
+
+class Angle(Constraint):
+    pass
+
+
+class Horizontal(Constraint):
+    pass
+
+
+class Vertical(Constraint):
+    pass
+
+
+class Fixed(Constraint):
+    pass
