86b28d30e17984270d21cedc46c9aec2ea33052e
Gleb Karpushkin <rugleb@gmail.com>
2018-10-25 19:33:02 +0300

    realized some constraints

--- cad/2d/geometry/constraints.py
+++ cad/2d/geometry/constraints.py
@@ -1,26 +1,102 @@
+from abc import abstractmethod
+
+from .objects import Line, Point
+
+
 class Constraint:
-    pass
+
+    @abstractmethod
+    def apply(self, obj):
+        pass
 
 
 class Coincident(Constraint):
-    pass
+
+    def __init__(self):
+        self.selected = None
+
+    def apply(self, obj):
+        if self.selected is None:
+            self.selected = obj
+        else:
+            if isinstance(obj, Point):
+                if isinstance(self.selected, Point):
+                    obj.setX(self.selected.x())
+                    obj.setY(self.selected.y())
 
 
 class Parallel(Constraint):
-    pass
+
+    def __init__(self):
+        self.selected = None
+
+    def apply(self, obj):
+        if isinstance(obj, Line):
+            if self.selected is None:
+                self.selected = obj
+            else:
+                angle = self.selected.angle()
+                obj.setAngle(angle)
+                self.selected = None
+
+
+class Perpendicular(Constraint):
+
+    def __init__(self):
+        self.selected = None
+
+    def apply(self, obj):
+        if isinstance(obj, Line):
+            if self.selected is None:
+                self.selected = obj
+            else:
+                angle = self.selected.angle()
+                obj.setAngle(angle + 90)
+                self.selected = None
 
 
 class Angle(Constraint):
-    pass
+
+    def __init__(self, value):
+        self.value = None
+
+        self.setValue(value)
+
+    def setValue(self, value):
+        self.value = value
+
+    def apply(self, obj):
+        if isinstance(obj, Line):
+            obj.setAngle(self.value)
 
 
 class Horizontal(Constraint):
-    pass
+
+    def apply(self, obj):
+        if isinstance(obj, Line):
+            obj.setAngle(0)
 
 
 class Vertical(Constraint):
-    pass
+
+    def apply(self, obj):
+        if isinstance(obj, Line):
+            obj.setAngle(90)
 
 
-class Fixed(Constraint):
-    pass
+class Length(Constraint):
+
+    def __init__(self, value):
+        self.value = None
+
+        self.setValue(value)
+
+    def setValue(self, value):
+        if value < 0:
+            message = 'Given value is invalid. Expected positive value'
+            raise Exception(message)
+        self.value = value
+
+    def apply(self, obj):
+        if isinstance(obj, Line):
+            obj.setLength(self.value)
