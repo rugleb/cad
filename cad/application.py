2040bb93f9eb75e8ee6d39edb2b07a4c9f522714
Gleb Karpushkin <rugleb@gmail.com>
2018-10-27 13:44:01 +0300

    disabled protected notations

--- cad/application.py
+++ cad/application.py
@@ -12,66 +12,66 @@ class Application(QMainWindow):
 
         self.sketch = None
 
-        self.__menu = None
-        self.__toolBar = None
-        self.__toolBarGroup = None
+        self.menu = None
+        self.toolBar = None
+        self.toolBarGroup = None
 
-        self.__initSketch()
-        self.__initMenuBar()
-        self.__initToolBar()
-        self.__initStatusBar()
-        self.__initGeometry()
+        self.initSketch()
+        self.initMenuBar()
+        self.initToolBar()
+        self.initStatusBar()
+        self.initGeometry()
 
         self.setWindowTitle('Sketch')
 
-    def __initSketch(self):
+    def initSketch(self):
         self.sketch = Sketch(self)
         self.setCentralWidget(self.sketch)
 
-    def __initMenuBar(self):
-        self.__menu = self.menuBar()
+    def initMenuBar(self):
+        self.menu = self.menuBar()
 
-        file = self.__menu.addMenu('File')
+        file = self.menu.addMenu('File')
         file.addAction(self.exitAction())
         file.addAction(self.openAction())
         file.addAction(self.saveAction())
 
-        edit = self.__menu.addMenu('Edit')
+        edit = self.menu.addMenu('Edit')
         edit.addAction(self.undoAction())
         edit.addAction(self.copyAction())
         edit.addAction(self.pasteAction())
         edit.addAction(self.deleteAction())
 
     def undoAction(self):
-        action = QAction('Undo', self.__menu)
+        action = QAction('Undo', self.menu)
         action.setShortcut('Ctrl+Z')
         action.setStatusTip('Undo')
         action.setToolTip('Undo')
         return action
 
     def copyAction(self):
-        action = QAction('Copy', self.__menu)
+        action = QAction('Copy', self.menu)
         action.setShortcut('Ctrl+C')
         action.setStatusTip('Copy')
         action.setToolTip('Copy')
         return action
 
     def pasteAction(self):
-        action = QAction('Paste', self.__menu)
+        action = QAction('Paste', self.menu)
         action.setShortcut('Ctrl+V')
         action.setStatusTip('Paste')
         action.setToolTip('Paste')
         return action
 
     def deleteAction(self):
-        action = QAction('Delete', self.__menu)
+        action = QAction('Delete', self.menu)
         action.setShortcut('Delete')
         action.setStatusTip('Delete')
         action.setToolTip('Delete')
         return action
 
     def exitAction(self):
-        action = QAction('Exit', self.__menu)
+        action = QAction('Exit', self.menu)
         action.setShortcut('Ctrl+Q')
         action.setToolTip('Close application')
         action.setStatusTip('Close application')
@@ -79,7 +79,7 @@ class Application(QMainWindow):
         return action
 
     def saveAction(self):
-        action = QAction('Save As', self.__menu)
+        action = QAction('Save As', self.menu)
         action.setShortcut('Ctrl+S')
         action.setToolTip('Save current application')
         action.setStatusTip('Save current application')
@@ -87,16 +87,16 @@ class Application(QMainWindow):
         return action
 
     def openAction(self):
-        action = QAction('Open', self.__menu)
+        action = QAction('Open', self.menu)
         action.setShortcut('Ctrl+O')
         action.setToolTip('Open file')
         action.setStatusTip('Open file')
         action.triggered.connect(self.showOpenDialog)
         return action
 
-    def __initToolBar(self):
-        self.__toolBar = self.addToolBar('Drawing')
-        self.__toolBarGroup = QActionGroup(self.__toolBar)
+    def initToolBar(self):
+        self.toolBar = self.addToolBar('Drawing')
+        self.toolBarGroup = QActionGroup(self.toolBar)
 
         default = self.disableScopeAction()
 
@@ -115,9 +115,9 @@ class Application(QMainWindow):
 
         for action in actions:
             action.setCheckable(True)
-            action.setParent(self.__toolBar)
-            self.__toolBar.addAction(action)
-            self.__toolBarGroup.addAction(action)
+            action.setParent(self.toolBar)
+            self.toolBar.addAction(action)
+            self.toolBarGroup.addAction(action)
 
         default.setChecked(True)
 
@@ -249,15 +249,15 @@ class Application(QMainWindow):
         return action
 
     def disableScopes(self):
-        for action in self.__toolBarGroup.actions():
+        for action in self.toolBarGroup.actions():
             action.setChecked(False)
         self.sketch.disableScope()
-        self.__toolBarGroup.actions()[0].setChecked(True)
+        self.toolBarGroup.actions()[0].setChecked(True)
 
-    def __initStatusBar(self):
+    def initStatusBar(self):
         self.statusBar().showMessage('Ready')
 
-    def __initGeometry(self):
+    def initGeometry(self):
         desktop = QDesktopWidget()
         self.setGeometry(desktop.availableGeometry())
 
