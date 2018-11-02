2c1da85fd6839e7c13b59486fe1d094d3b4cf3e5
Gleb Karpushkin <rugleb@gmail.com>
2018-11-02 12:34:25 +0300

    added type hints

--- cad/geometry/figures.py
+++ cad/geometry/figures.py
@@ -12,23 +12,23 @@ class Figure(ABC):
         self._name = None
         self._constraints = []
 
-    def setName(self, name):
+    def setName(self, name: str) -> None:
         self._checkName(name)
         self._name = name
 
-    def getName(self):
+    def getName(self) -> str:
         return self._name
 
-    def hasName(self):
+    def hasName(self) -> bool:
         return type(self._name) is not None
 
     @classmethod
-    def _checkName(cls, name):
+    def _checkName(cls, name: str) -> None:
         if type(name) is not str:
             msg = 'The figure name must be an instance of string'
             raise GivenTypeIsInvalidException(msg)
 
-    def setConstraint(self, constraint):
+    def setConstraint(self, constraint: Constraint) -> None:
         # Let's check the object for normal type
         self._checkConstraint(constraint)
 
@@ -42,15 +42,15 @@ class Figure(ABC):
         self._constraints.append(constraint)
 
     @classmethod
-    def _checkConstraint(cls, constraint):
+    def _checkConstraint(cls, constraint: Constraint) -> None:
         if type(constraint) is not Constraint:
             msg = 'The constraint must be an instance of the Constraint class'
             raise GivenTypeIsInvalidException(msg)
 
-    def getConstraints(self):
+    def getConstraints(self) -> list:
         return self._constraints
 
-    def hasConstraints(self):
+    def hasConstraints(self) -> bool:
         return [] == self.getConstraints()
 
 
@@ -62,27 +62,27 @@ class PointFigure(Figure):
         self._x = None
         self._y = None
 
-    def setX(self, x):
+    def setX(self, x: float) -> None:
         self._checkCoordinate(x)
         self._x = x
 
-    def getX(self):
+    def getX(self) -> float:
         return self._x
 
-    def setY(self, y):
+    def setY(self, y: float) -> None:
         self._checkCoordinate(y)
         self._y = y
 
-    def getY(self):
+    def getY(self) -> float:
         return self._y
 
     @classmethod
-    def _checkCoordinate(cls, coordinate):
+    def _checkCoordinate(cls, coordinate) -> None:
         if type(coordinate) not in (float, int):
             msg = 'The point coordinate must be an instance of int or float'
             raise GivenTypeIsInvalidException(msg)
 
-    def toQtPoint(self):
+    def toQtPoint(self) -> QPointF:
         x = self.getX()
         y = self.getY()
         return QPointF(x, y)
@@ -96,45 +96,45 @@ class LineFigure(Figure):
         self._p1 = None
         self._p2 = None
 
-    def setP1(self, point):
+    def setP1(self, point: PointFigure) -> None:
         self._checkPoint(point)
         self._p1 = point
 
-    def getP1(self):
+    def getP1(self) -> PointFigure:
         return self._p1
 
-    def hasP1(self):
+    def hasP1(self) -> bool:
         return self.getP1() is not None
 
-    def setP2(self, point):
+    def setP2(self, point: PointFigure) -> None:
         self._checkPoint(point)
         self._p2 = point
 
-    def getP2(self):
+    def getP2(self) -> PointFigure:
         return self._p2
 
-    def hasP2(self):
+    def hasP2(self) -> float:
         return self.getP2() is not None
 
-    def getX1(self):
+    def getX1(self) -> float:
         return self.getP1().getX()
 
-    def getY1(self):
+    def getY1(self) -> float:
         return self.getP1().getY()
 
-    def getX2(self):
+    def getX2(self) -> float:
         return self.getP2().getX()
 
-    def getY2(self):
+    def getY2(self) -> float:
         return self.getP2().getY()
 
     @classmethod
-    def _checkPoint(cls, point):
+    def _checkPoint(cls, point) -> None:
         if type(point) is not PointFigure:
             msg = 'The point must be an instance of the PointFigure class'
             raise GivenTypeIsInvalidException(msg)
 
-    def toQtLine(self):
+    def toQtLine(self) -> QLineF:
         p1 = self.getP1()
         p2 = self.getP2()
         return QLineF(p1, p2)
