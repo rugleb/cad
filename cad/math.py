69b15eb2c3c902cf24304f511d9f1e0ab0792bd9
Gleb Karpushkin <rugleb@gmail.com>
2018-11-16 14:36:21 +0300

    deleted drawing module

--- cad/math.py
+++ cad/math.py
@@ -5,18 +5,18 @@ def distancePointToPoint(p1: QPointF, p2: QPointF) -> float:
     return QLineF(p1, p2).length()
 
 
-def distancePointToVector(p: QPointF, l: QLineF):
+def distancePointToVector(p: QPointF, l: QLineF) -> float:
     if not l.length() > 0:
         return distancePointToPoint(p, l.p1())
     s = l.dy() * p.x() - l.dx() * p.y() + l.x2() * l.y1() - l.y2() * l.x1()
     return abs(s) / l.length()
 
 
-def isPointOnVector(p: QPointF, l: QLineF):
+def isPointOnVector(p: QPointF, l: QLineF) -> bool:
     return distancePointToVector(p, l) == 0.
 
 
-def isPointOnLine(p: QPointF, l: QLineF):
+def isPointOnLine(p: QPointF, l: QLineF) -> bool:
     if isPointOnVector(p, l):
         return l.x1() <= p.x() <= l.x2()
     return False
--- cad/sketch.py
+++ cad/sketch.py
@@ -1,7 +1,5 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-from cad.drawing import Pen
-
 
 class Sketch(QtWidgets.QWidget):
 
