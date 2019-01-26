f039cbd8920727d90a64da86a9882873cc614ffc
Gleb Karpushkin <rugleb@gmail.com>
2019-01-27 00:43:55 +0300

    created launch.py file

---
+++ launch.py
@@ -0,0 +1,11 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+
+from cad.application import Application
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    workspace = Application()
+    workspace.show()
+    sys.exit(app.exec_())
