86b41dc606d1f8f1b215d63d56ae0c954d3257ba
Gleb Karpushkin <rugleb@gmail.com>
2019-07-17 15:14:19 +0300

    created test on Fixing constraint

--- tests/test_constraints.py
+++ tests/test_constraints.py
@@ -2,6 +2,7 @@ import unittest
 
 from cad.algebra import Point, Solver, p2p
 from cad.algebra import CoincidentY, CoincidentX, Length
+from cad.algebra import FixingX, FixingY
 
 
 class ConstraintTestCase(unittest.TestCase):
@@ -22,6 +23,16 @@ class ConstraintTestCase(unittest.TestCase):
     def assertCoincident(self, p1: Point, p2: Point):
         self.assertCoincidentX(p1, p2)
         self.assertCoincidentY(p1, p2)
+
+    def assertFixingX(self, point: Point, lock: Point):
+        self.assertEqual(point.x(), lock.x())
+
+    def assertFixingY(self, point: Point, lock: Point):
+        self.assertEqual(point.y(), lock.y())
+
+    def assertFixing(self, point: Point, lock: Point):
+        self.assertFixingX(point, lock)
+        self.assertFixingY(point, lock)
 
 
 class LengthConstraintTestCase(ConstraintTestCase):
@@ -57,6 +68,23 @@ class CoincidentConstraintTestCase(ConstraintTestCase):
 
         p1, p2 = self.solver.recount()
         self.assertCoincident(p1, p2)
+
+
+class FixingConstraintTestCase(ConstraintTestCase):
+
+    def test_fixing(self):
+        point = Point(10, 20)
+        self.solver.addPoint(point)
+
+        x, y = 15, 15
+        constraintX = FixingX(point, x)
+        constraintY = FixingY(point, y)
+
+        self.solver.addConstraint(constraintX)
+        self.solver.addConstraint(constraintY)
+
+        point,  = self.solver.recount()
+        self.assertFixing(point, Point(x, y))
 
 
 if __name__ == '__main__':
