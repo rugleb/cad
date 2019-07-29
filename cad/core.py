34b57df35387efb40dd9ae085aeea86e21713041
Gleb Karpushkin <rugleb@gmail.com>
2019-07-30 00:11:46 +0300

    extended Drawable interface

--- cad/core.py
+++ cad/core.py
@@ -2,8 +2,8 @@ from __future__ import annotations
 
 from abc import abstractmethod
 
-from PySide2.QtGui import QPainter, QColor, QPen
-from PySide2.QtCore import QPointF, Qt
+from PySide2.QtGui import QPainter, QColor, QPen, QBrush
+from PySide2.QtCore import QPointF
 
 Point = QPointF
 Color = QColor
@@ -20,6 +20,10 @@ class Pen(QPen):
     pass
 
 
+class Brush(QBrush):
+    pass
+
+
 class Painter(QPainter):
 
     def drawCircle(self, center: Point, radius: float) -> None:
@@ -27,7 +31,95 @@ class Painter(QPainter):
 
 
 class Drawable:
+    """The Drawable interface.
+
+    This class defines the interface for manipulation of CAD drawable objects.
+    """
+
+    def color(self) -> Color:
+        """Return the color of the invoking object.
+
+        :return: The color of the object
+        :rtype: QColor
+        """
+
+        pass
+
+    def setColor(self, color: Color) -> None:
+        """Edit the color of the object.
+
+        :param QColor color: A new color object
+        :return: None
+        """
+
+        pass
+
+    def width(self) -> float:
+        """Return the drawing brush width of the object.
+
+        :return: The width of the brush
+        :rtype: float
+        """
+
+        pass
+
+    def setWidth(self, width: float) -> float:
+        """Edit the line width of the object.
+
+        :param float width: New brush width
+        :return: None
+        """
+
+        pass
+
+    def highlight(self) -> None:
+        """Highlight the object.
+
+        :return: None
+        """
+
+        pass
+
+    def unHighlight(self) -> None:
+        """Unhighlight the invoking object.
+
+        :return: None
+        """
+
+        pass
+
+    def isHighlighted(self) -> bool:
+        """Determine if the object is highlighted.
+
+        :return: None
+        """
+
+        pass
+
+    def pen(self) -> Pen:
+        """Returns the Pen class instance.
+
+        :return: Instance of Pen class
+        :rtype: Pen
+        """
+
+        pass
+
+    def brush(self) -> Brush:
+        """Returns the Brush class instance.
+
+        :return: Instance of Brush class
+        :rtype: Pen
+        """
+
+        pass
 
     @abstractmethod
     def draw(self, painter: Painter) -> None:
+        """Draws the object by given Painter.
+
+        :param Painter painter:
+        :return: None
+        """
+
         pass
