c5ebdbca9850257966bf2cd4a67924df425955d2
Gleb Karpushkin <rugleb@gmail.com>
2019-08-07 13:06:17 +0300

    updated p2l method

--- cad/algebra.py
+++ cad/algebra.py
@@ -27,16 +27,23 @@ def p2p(p1: Point, p2: Point) -> float:
     return Line(p1, p2).length()
 
 
-def p2l(point: Point, line: Point, rounded: int = ROUNDED) -> float:
+def p2l(point: Point, line: Line) -> float:
+    """Returns the distance between point and line.
+
+    :param Point point:
+    :param Line line:
+    :return: distance between point and line
+    :rtype: float
+    """
+
     if line.length() > 0:
         x0, y0 = point.x(), point.y()
         x1, y1 = line.x1(), line.y1()
         x2, y2 = line.x2(), line.y2()
         square = (y2 - y1) * x0 - (x2 - x1) * y0 + x2 * y1 - y2 * x1
-        dist = np.abs(square) / line.length()
-        return np.round(dist, rounded)
-    else:
-        return p2p(line.p1(), point)
+        return np.abs(square) / line.length()
+
+    return p2p(line.p1(), point)
 
 
 def p2s(point: Point, line: Line, rounded: int = ROUNDED) -> float:
