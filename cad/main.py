7ca3a66b88be7a622dd7e75002adb661e741954a
Gleb Karpushkin <rugleb@gmail.com>
2019-08-11 23:03:14 +0300

    integrated Horizontal constraint

--- cad/main.py
+++ cad/main.py
@@ -7,7 +7,7 @@ from PySide2 import QtWidgets, QtGui, QtCore
 from cad.log import logger
 from cad.core import Point, SmartPoint, SmartSegment, Painter
 from cad.constraints import Parallel, Perpendicular, Length, CoincidentX, \
-    CoincidentY
+    CoincidentY, Horizontal
 
 
 def iconPath(name: str) -> str:
@@ -820,5 +820,14 @@ class HorizontalController(Controller):
 
         self.sketch.mousePressed.connect(self.onMousePressed)
 
-    def onMousePressed(self, point: Point) -> None:
-        pass
+    def onMousePressed(self, cursor: Point) -> None:
+        try:
+            segment = self.sketch.closestSegment(cursor)
+
+            p1, p2 = segment.segment().points()
+            constraint = Horizontal(p1, p2)
+
+            logger.debug('Horizontal constraint created')
+
+        except KeyError:
+            pass
